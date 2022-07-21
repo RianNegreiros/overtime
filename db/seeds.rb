7bd3080cc4b174775d9671b13d7fda4d9519e88b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-21 18:59:28 -0300

    remove user creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-User.create!(email: "any@test.com", password: "123456", password_confirmation: "123456", first_name: "any", last_name: "any")
-
-puts "User has been created"
-
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content")
 end
