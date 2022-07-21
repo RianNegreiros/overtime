91abbb0ac6db6cf690fbae1750d21ef12023c19a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-21 16:11:09 -0300

    sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+@user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", fist_name: "any", last_name: "any")
+
+puts "User has been created"
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+end
+
+puts "100 Posts have been created"
