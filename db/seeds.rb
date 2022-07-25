bed180c18c487ffa5b5a9c84c172561b389c889f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-25 14:18:09 -0300

    add user to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 AdminUser.create(email: "admin@mail.com", password: "123456", password_confirmation: "123456", first_name: "admin", last_name: "user")
 
 puts "1 Admin User created"
+
+User.create(email: "user@mail.com", password: "123456", password_confirmation: "123456", first_name: "user", last_name: "user")
+
+puts "1 User created"
