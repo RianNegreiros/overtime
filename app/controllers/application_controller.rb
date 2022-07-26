922ba6185a25baa42825eca64127791f9c3b7cf8
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-26 09:58:46 -0300

    fix deprecation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include Pundit
+  include Pundit::Authorization
   before_action :authenticate_user!
   add_flash_types :danger, :info, :warning, :success, :messages
 end
