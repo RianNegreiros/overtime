eddc1f46f6db7ac8288f3a76be07d8a01361b25b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-26 12:16:54 -0300

    get only posts created by the current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update]
 
   def index
-    @posts = Post.all
+    @posts = Post.post_by current_user
   end
 
   def new
