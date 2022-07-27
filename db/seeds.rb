d2655c7c8968932dae27d7b9dc1d5b0c55c1e63f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-27 17:31:30 -0300

    add pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@ AdminUser.create(email: "admin@mail.com", password: "123456", password_confirmat
 
 puts "1 Admin User created"
 
-User.create(email: "user@mail.com", password: "123456", password_confirmation: "123456", first_name: "user", last_name: "user", phone: "111111111111")
+@user = User.create(email: "user@mail.com", password: "123456", password_confirmation: "123456", first_name: "user", last_name: "user", phone: "111111111111")
 
 puts "1 User created"
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+end
+
+puts "100 Posts have been created"
