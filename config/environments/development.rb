e7b6f969c40255b29a260a58d25560a49dbcb4b5
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-28 14:17:34 -0300

    mailer functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,13 +16,16 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
-  config.active_storage.service = :local
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.active_support.deprecation = :log
   config.active_support.disallowed_deprecation = :raise
   config.active_support.disallowed_deprecation_warnings = []
+
+  config.active_storage.service = :local
   config.active_record.migration_error = :page_load
   config.active_record.verbose_query_logs = true
   config.assets.quiet = true
