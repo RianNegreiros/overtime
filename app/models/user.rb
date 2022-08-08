65ad6fe65c0d1d0036c2c524e63ca7be8b23cd6b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-08-08 15:01:58 -0300

    relationship between managers and employees

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,19 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :audit_logs
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :hands_associations, class_name: 'Hand'
+  has_many :hands, through: :hands_associations
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates_presence_of :first_name, :last_name, :phone
+  validates_presence_of :first_name, :last_name, :phone, :ssn, :company
 
   PHONE_REGEX = /\A\d*\Z/
 
   validates_format_of :phone, with: PHONE_REGEX
 
   validates :phone, length: { is: 12 }
-
-  scope :employees, -> { where(type: nil) }
 
   def full_name
     last_name.upcase + ", " + first_name.upcase
