1a37d3db44b6a99436a6e3713bb048771ed245f9
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-08-09 11:33:32 -0300

    add protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+  protect_from_forgery with: :exception
   before_action :authenticate_user!
   add_flash_types :danger, :info, :warning, :success, :messages
 
