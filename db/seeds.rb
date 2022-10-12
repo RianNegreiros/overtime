8e3c9d6fcf3f0c107477e591ae1dd9766df3aaca
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-12 07:12:24 -0300

    change emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-AdminUser.create(email: "admin@mail.com", password: "123456", password_confirmation: "123456", first_name: "admin", last_name: "user", phone: "111111111111")
+AdminUser.create(email: "admin@test.com", password: "123456", password_confirmation: "123456", first_name: "admin", last_name: "user", phone: "111111111111")
 
 puts "1 Admin User created"
 
-@employee = Employee.create(email: "user@mail.com", password: "123456", password_confirmation: "123456", first_name: "user", last_name: "user", phone: "111111111111")
+@employee = Employee.create(email: "user@test.com", password: "123456", password_confirmation: "123456", first_name: "user", last_name: "user", phone: "111111111111")
 
 puts "1 User created"
 
